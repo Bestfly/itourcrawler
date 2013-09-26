6a18d0d2fcdb32eb29733d2ce4ee508f52c0c9e9
baina <baina@189.cn>
2013-09-26 14:47:27 +0800

    debug g.lua

--- ngx/g.lua
+++ ngx/g.lua
@@ -14,6 +14,8 @@ function error000 (mes)
 end
 local cloudfetch = {}
 table.insert(cloudfetch, "bestfly")
+table.insert(cloudfetch, "bestfly")
+--[[
 table.insert(cloudfetch, "deekpark")
 table.insert(cloudfetch, "huangqi")
 table.insert(cloudfetch, "mpicloud")
@@ -23,33 +25,13 @@ table.insert(cloudfetch, "taowap")
 table.insert(cloudfetch, "faceba")
 table.insert(cloudfetch, "jijilu")
 table.insert(cloudfetch, "cloudavh")
+--]]
 local baseurl = "http://%s.duapp.com%s";
 ngx.say(type(ngx.var.uri));
 local index = string.find(ngx.var.uri, "gw");
 local tmpuri = string.sub(ngx.var.uri, index+2, -1);
 -- local tmpuri = "/tz.php"
-if ngx.var.request_method == "GET" then
-	local tmprandom = math.random(1,10);
-	-- tmpuri = string.gsub(tmpuri, "/data-gw/", "/")
-	ngx.say(tmpuri)
-	ngx.say(string.format(baseurl, cloudfetch[tmprandom], tmpuri))
-	local hc = http:new()
-	local ok, code, headers, status, body = hc:request {
-		url = string.format(baseurl, cloudfetch[tmprandom], tmpuri),
-		-- proxy = "http://" .. ngx.decode_base64(ngx.var.proxy),
-		timeout = 3000,
-		method = "GET", -- POST or GET
-		-- add post content-type and cookie
-		headers = { ["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6"},
-		-- body = ltn12.source.string(form_data),
-		-- body = form_data,
-	}
-	if code == 200 and body ~= nil then
-		ngx.print(body);
-	else
-		ngx.print(error000(code, status))
-	end
-else
+if ngx.var.request_method == "POST" then
 	-- ngx.exit(ngx.HTTP_FORBIDDEN);
 	ngx.req.read_body();
 	-- local uargs = ngx.req.get_uri_args();
@@ -57,7 +39,7 @@ else
 	-- local phead = ngx.req.get_headers();
 	local pcontent = ngx.req.get_body_data();
 	if pcontent then
-		local tmprandom = math.random(1,10);
+		local tmprandom = math.random(1,2);
 		local hc = http:new()
 		local ok, code, headers, status, body = hc:request {
 			url = string.format(baseurl, cloudfetch[tmprandom], tmpuri) .. "?" .. ngx.var.args,
@@ -66,7 +48,7 @@ else
 			method = "POST", -- POST or GET
 			-- add post content-type and cookie
 			-- headers = phead,
-			headers = { ["Host"] = cloudfetch[tmprandom] .. ".duapp.com", ["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6" },
+			headers = { ["Host"] = cloudfetch[tmprandom] .. ".duapp.com" },
 			-- body = ltn12.source.string(form_data),
 			body = pcontent,
 		}
@@ -90,5 +72,27 @@ else
 		end
 	else
 		ngx.print(0)
+	end
+else
+	local tmprandom = math.random(1,2);
+	-- tmpuri = string.gsub(tmpuri, "/data-gw/", "/")
+	ngx.say(tmpuri)
+	ngx.say(string.format(baseurl, cloudfetch[tmprandom], tmpuri))
+	local hc = http:new()
+	local ok, code, headers, status, body = hc:request {
+		url = string.format(baseurl, cloudfetch[tmprandom], tmpuri),
+		-- proxy = "http://" .. ngx.decode_base64(ngx.var.proxy),
+		timeout = 3000,
+		method = "GET", -- POST or GET
+		-- add post content-type and cookie
+		headers = { ["Host"] = cloudfetch[tmprandom] .. ".duapp.com" },
+		-- headers = { ["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6"},
+		-- body = ltn12.source.string(form_data),
+		-- body = form_data,
+	}
+	if code == 200 and body ~= nil then
+		ngx.print(body);
+	else
+		ngx.print(error000(code, status))
 	end
 end
